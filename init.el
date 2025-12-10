475dc686cfb60db2add4ec30214ca5335ac8ec2f
Sebastian Schubert <sschubert@initse.com>
2025-12-10 10:30:33 +0100

    Switched to nano ui, enabled global editorconfig mode

--- init.el
+++ init.el
@@ -74,8 +74,8 @@
   ;; :custom
   :config
   (require 'nano)
-  (require 'nano-theme)
-  (load-theme 'nano-theme-dark))
+  (require 'nano-faces)
+  (require 'nano-theme))
 
 ;;; ------------------------------------------------------------
 ;;; Environment Variables (important for macOS)
@@ -128,130 +128,11 @@
 ;; variants of basic, built-in functionality.  One of the headline
 ;; features of `consult' is its preview facility, where it shows in
 ;; another Emacs window the context of what is currently matched in
-;; the minibuffer.  Here I define key bindings for some commands you
-;; may find useful.  The mnemonic for their prefix is "alternative
-;; search" (as opposed to the basic C-s or C-r keys).
-;;
-;; Further reading: https://protesilaos.com/emacs/dotemacs#h:22e97b4c-d88d-4deb-9ab3-f80631f9ff1d
-(use-package consult
-  :bind (;; A recursive grep
-         ("M-s M-g" . consult-grep)
-         ;; Search for files names recursively
-         ("M-s M-f" . consult-find)
-         ;; Search through the outline (headings) of the file
-         ("M-s M-o" . consult-outline)
-         ;; Search the current buffer
-         ("M-s M-l" . consult-line)
-         ;; Switch to another buffer, or bookmarked file, or recently
-         ;; opened file.
-         ("M-s M-b" . consult-buffer)))
-
-;; The `embark' package lets you target the thing or context at point
-;; and select an action to perform on it.  Use the `embark-act'
-;; command while over something to find relevant commands.
-;;
-;; When inside the minibuffer, `embark' can collect/export the
-;; contents to a fully fledged Emacs buffer.  The `embark-collect'
-;; command retains the original behaviour of the minibuffer, meaning
-;; that if you navigate over the candidate at hit RET, it will do what
-;; the minibuffer would have done.  In contrast, the `embark-export'
-;; command reads the metadata to figure out what category this is and
-;; places them in a buffer whose major mode is specialised for that
-;; type of content.  For example, when we are completing against
-;; files, the export will take us to a `dired-mode' buffer; when we
-;; preview the results of a grep, the export will put us in a
-;; `grep-mode' buffer.
-;;
-;; Further reading: https://protesilaos.com/emacs/dotemacs#h:61863da4-8739-42ae-a30f-6e9d686e1995
-(use-package embark
-  :bind (("C-." . embark-act)
-         :map minibuffer-local-map
-         ("C-c C-c" . embark-collect)
-         ("C-c C-e" . embark-export)))
-
-;; The `embark-consult' package is glue code to tie together `embark'
-;; and `consult'.
-(use-package embark-consult)
-
-;; The `wgrep' packages lets us edit the results of a grep search
-;; while inside a `grep-mode' buffer.  All we need is to toggle the
-;; editable mode, make the changes, and then type C-c C-c to confirm
-;; or C-c C-k to abort.
-;;
-;; Further reading: https://protesilaos.com/emacs/dotemacs#h:9a3581df-ab18-4266-815e-2edd7f7e4852
-(use-package wgrep
-  :bind ( :map grep-mode-map
-          ("e" . wgrep-change-to-wgrep-mode)
-          ("C-x C-q" . wgrep-change-to-wgrep-mode)
-          ("C-c C-c" . wgrep-finish-edit)))
-
-(savehist-mode 1)
-
-(recentf-mode 1)
-
-
-;;; ------------------------------------------------------------
-;;; Rojectile
-;;; ------------------------------------------------------------
-(use-package projectile
-  :diminish projectile-mode
-  :config (projectile-mode)
-  :bind-keymap
-  ("C-c p" . projectile-command-map)
-  :init
-  (when (file-directory-p "~/source")
-    (setq projectile-project-search-path '("~/source" . 1)))
-  (setq projectile-switch-project-action #'projectile-find-file))
-
-;;; ------------------------------------------------------------
-;;; Company
-;;; ------------------------------------------------------------
-
-;; Company is the best Emacs completion system.
-(use-package company
-  :init
-  (global-company-mode)
-
   :custom
-  ;; Make Company less intrusive and more like modern IDE autocomplete
-  (company-idle-delay 0.05)        ;; fast popup
-  (company-minimum-prefix-length 1)
-  (company-tooltip-align-annotations t)
-  (company-tooltip-limit 12)
-  (company-tooltip-minimum-width 40)
-  (company-show-numbers t)
-  (company-require-match nil)
-  (company-dabbrev-other-buffers t)
-  (company-dabbrev-downcase nil)
-
-  :bind
-  (:map company-active-map
-        ("<tab>" . company-complete-selection)
-        ("TAB"   . company-complete-selection)
-        ("C-n"   . company-select-next)
-        ("C-p"   . company-select-previous))
-
-  :config
-  ;; Good backends for general programming
-  (setq company-backends
-        '((company-capf            ;; completion-at-point (LSP, modes, etc.)
-           company-dabbrev-code)   ;; fallback for code-like text
-          company-dabbrev))        ;; fallback for everything else
-  )
-
-;; Flycheck is the newer version of flymake and is needed to make lsp-mode not freak out.
-(use-package flycheck
-  :config
-  (add-hook 'prog-mode-hook 'flycheck-mode) ;; always lint my code
-  (add-hook 'after-init-hook #'global-flycheck-mode))
-
-;; Package for interacting with language servers
-(use-package lsp-mode
-  :commands lsp
-  :config
   (setq lsp-prefer-flymake nil ;; Flymake is outdated
-        lsp-headerline-breadcrumb-mode nil)) ;; I don't like the symbols on the header a-la-vscode, remove this if you like them.
+        lsp-headerline-breadcrumb-enable nil)) ;; I don't like the symbols on the header a-la-vscode, remove this if you like them.
 
+(use-package lsp-ui :commands lsp-ui-mode)
 (use-package which-key :config (which-key-mode))
 
 ;;; ------------------------------------------------------------
@@ -340,8 +221,27 @@
 (use-package expand-region
   :bind ("C-=" . er/expand-region))
 
+(use-package pulsar
+  :ensure t
+  :bind
+  ( :map global-map
+    ("C-x l" . pulsar-pulse-line) ; overrides `count-lines-page'
+    ("C-x L" . pulsar-highlight-permanently-dwim)) ; or use `pulsar-highlight-temporarily-dwim'
+  :init
+  (pulsar-global-mode 1)
+  :config
+  (setq pulsar-delay 0.055)
+  (setq pulsar-iterations 5)
+  (setq pulsar-face 'pulsar-green)
+  (setq pulsar-region-face 'pulsar-yellow)
+  (setq pulsar-highlight-face 'pulsar-magenta))
+
+(editorconfig-mode t)
 ;;; ------------------------------------------------------------
 ;;; Custom settings file
 ;;; ------------------------------------------------------------
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file t)
+
+(provide 'init)
+;;; init.el ends here

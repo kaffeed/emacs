5f4e212a39f70fed34f144f25b710eb5d14d9367
Sebastian Schubert <sschubert932@gmail.com>
2025-12-11 19:19:34 +0100

    Add native compilation per default

--- early-init.el
+++ early-init.el
@@ -1,4 +1,13 @@
-;;; early-init.el --- Early initialization
+;;; early-init.el --- Early initialization  -*- lexical-binding: t; -*-
+
+;; Ensure Emacs loads the most recent byte-compiled files.
+(setq load-prefer-newer t)
+
+;; Make Emacs Native-compile .elc files asynchronously by setting
+;; `native-comp-jit-compilation' to t.
+(setq native-comp-jit-compilation t)
+(setq native-comp-deferred-compilation native-comp-jit-compilation)  ; Deprecated
+
 
 ;; Disable package.el to use straight.el instead
 (setq package-enable-at-startup nil)
--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;;; ------------------------------------------------------------
+;;; ------------------------------------------------------------  -*- lexical-binding: t; -*-
 ;;; Package Setup
 ;;; ------------------------------------------------------------
 
@@ -51,6 +51,30 @@
 ;; Configure use-package to use straight.el by default
 (setq-default straight-use-package-by-default t
               use-package-verbose t)
+
+(use-package compile-angel
+  :demand t
+  :config
+  ;; Set `compile-angel-verbose' to nil to disable compile-angel messages.
+  ;; (When set to nil, compile-angel won't show which file is being compiled.)
+  (setq compile-angel-verbose t)
+
+  ;; Uncomment the line below to compile automatically when an Elisp file is saved
+  ;; (add-hook 'emacs-lisp-mode-hook #'compile-angel-on-save-local-mode)
+
+  ;; The following directive prevents compile-angel from compiling your init
+  ;; files. If you choose to remove this push to `compile-angel-excluded-files'
+  ;; and compile your pre/post-init files, ensure you understand the
+  ;; implications and thoroughly test your code. For example, if you're using
+  ;; the `use-package' macro, you'll need to explicitly add:
+  ;; (eval-when-compile (require 'use-package))
+  ;; at the top of your init file.
+  (push "/init.el" compile-angel-excluded-files)
+  (push "/early-init.el" compile-angel-excluded-files)
+
+  ;; A global mode that compiles .el files before they are loaded
+  ;; using `load' or `require'.
+  (compile-angel-on-load-mode 1))
 
 ;;; ------------------------------------------------------------
 ;;; OS Settings

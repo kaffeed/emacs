d0b92d71dc3d7fe6573470f17aca86fe285575ed
Sebastian Schubert <sschubert932@gmail.com>
2026-03-25 23:15:30 +0100

    feat: keybindings

--- init.el
+++ init.el
@@ -202,6 +202,26 @@ Otherwise, opens in the directory of the current file."
    ("<f8>" . magit-status)
    ("<f9>" . compile)
    ("<f12>" . eshell)
+   ("C-x w t"  . window-layout-transpose)            ; EMACS-31
+   ("C-x w r"  . window-layout-rotate-clockwise)     ; EMACS-31
+   ("C-x w f h"  . window-layout-flip-leftright)     ; EMACS-31
+   ("C-x w f v"  . window-layout-flip-topdown)       ; EMACS-31
+   ("C-x 5 l"  . select-frame-by-name)
+   ("C-x 5 s"  . set-frame-name)
+   ("RET" . newline-and-indent)
+   ("M-J" . duplicate-dwim)                          ; As suggest on r/emacs by the_cecep:
+   ("M-K" . kill-paragraph)                          ; Expands M-k for kill-sentence
+   ("M-Z" . zap-up-to-char)                          ; Expands M-z for zap-to-char
+   ("M-F" . forward-to-word)                         ; Expands M-f to jump to beginning of next word
+   ("M-B" . backward-to-word)                        ; Expands M-b to jump to end of previous word
+   ("M-M" . end-of-line)                             ; Expands M-m to jump to end line, useful for paragraphs
+   ("M-T" . transpose-sentences)                     ; Expands M-t for transposing words
+   ("C-x M-t" . transpose-paragraphs)                ; Expands C-x C-t for transposing lines
+   ([remap capitalize-word] . capitalize-dwim)       ; Make M-c work on regions
+   ([remap downcase-word] . downcase-dwim)           ; Make M-l work on regions
+   ([remap upcase-word] . upcase-dwim)               ; Make M-u work on regions
+   ([remap kill-buffer] . kill-current-buffer)       ; C-x k stops prompting for buffer to kill
+   ([remap delete-horizontal-space] . cycle-spacing) ; M-\. Called twice, cycle-spacing has same effect and its default binding (M-SPC) is problematic in macOS
    ("C-x <up>" . windmove-up)
    ("C-x <down>" . windmove-down)
    ("C-x <left>" . windmove-left)
@@ -251,21 +271,34 @@ Otherwise, opens in the directory of the current file."
 ;;; ------------------------------------------------------------
 ;;; OS Settings
 
-(use-package standard-themes
-  :init
-  (standard-themes-take-over-modus-themes-mode 1)
-  :bind
-  (("C-<f5>" . modus-themes-rotate)
-   ("M-<f5>" . modus-themes-select)
-   ("C-M-<f5>" . modus-themes-load-random))
-  :config
-  (setq modus-themes-mixed-fonts t)
-  (setq modus-themes-italic-constructs t)
-  (modus-themes-load-theme 'standard-light))
-
 ;;; ------------------------------------------------------------
 ;;; Appearance
 ;;; ------------------------------------------------------------
+(use-package ef-themes
+  :ensure t
+  :init
+  ;; This makes the Modus commands listed below consider only the Ef
+  ;; themes.  For an alternative that includes Modus and all
+  ;; derivative themes (like Ef), enable the
+  ;; `modus-themes-include-derivatives-mode' instead.  The manual of
+  ;; the Ef themes has a section that explains all the possibilities:
+  ;;
+  ;; - Evaluate `(info "(ef-themes) Working with other Modus themes or taking over Modus")'
+  ;; - Visit <https://protesilaos.com/emacs/ef-themes#h:6585235a-5219-4f78-9dd5-6a64d87d1b6e>
+  (ef-themes-take-over-modus-themes-mode 1)
+  :bind
+  (("<f5>" . modus-themes-rotate)
+   ("C-<f5>" . modus-themes-select)
+   ("M-<f5>" . modus-themes-load-random))
+  :config
+  ;; All customisations here.
+  (setq modus-themes-mixed-fonts t)
+  (setq modus-themes-italic-constructs t)
+
+  ;; Finally, load your theme of choice (or a random one with
+  ;; `modus-themes-load-random', `modus-themes-load-random-dark',
+  ;; `modus-themes-load-random-light').
+  (modus-themes-load-theme 'ef-summer))
 
 ;;; ------------------------------------------------------------
 ;;; Environment Variables (important for macOS)
